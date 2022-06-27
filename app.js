5623a935f2baccfae272e024e80cd2f454ca8bad
clickjaw <clickjaw@outlook.com>
2022-06-27 13:52:28 -0500

    Just the css

---
+++ app.js
@@ -0,0 +1,30 @@
+// "use strict;"
+
+
+// let calculator = document.getElementById('calculator');
+// let tipAmount = document.getElementById('tip-amount');
+// let totalAmount = document.getElementById('total-amount');
+
+
+
+
+// function submitForm(e){
+//     e.preventDefault() //resets behavior
+//     let tipAmountPrint = document.getElementById('tipAmount');
+//     tipAmountPrint.textContent = 
+
+    
+//     let totalAmountPrint = document.getElementById('totalAmount');
+//     let totalAmountPrint.textContent = 
+
+
+//    `First Name: ${document.getElementById('firstName').value}
+//     Last Name: ${document.getElementById('lastName').value}
+//     Username: ${document.getElementById('userName').value}
+//     Password: ${document.getElementById('password').value}
+//     Email: ${document.getElementById('email').value}
+//     Phone Number: ${document.getElementById('phoneNumber').value}
+//     City: ${document.getElementById('cityMenu').value}
+//    `;
+//     my_form.reset() //resets the form
+// }
