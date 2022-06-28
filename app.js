8a9222f2bc624af4c83dee1e8a81f50bb85d1bd4
clickjaw <clickjaw@outlook.com>
2022-06-28 14:17:20 -0500

    nothing broke come HTML

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-// "use strict;"
+'use strict';
 
 
-// let calculator = document.getElementById('calculator');
-// let tipAmount = document.getElementById('tip-amount');
-// let totalAmount = document.getElementById('total-amount');
+document.querySelector('#submit-button').addEventListener('click', function(){
 
+document.querySelector('#tip-amount-number').textContent = '15';
 
-
-
-// function submitForm(e){
-//     e.preventDefault() //resets behavior
-//     let tipAmountPrint = document.getElementById('tipAmount');
-//     tipAmountPrint.textContent = 
-
-    
-//     let totalAmountPrint = document.getElementById('totalAmount');
-//     let totalAmountPrint.textContent = 
-
-
-//    `First Name: ${document.getElementById('firstName').value}
-//     Last Name: ${document.getElementById('lastName').value}
-//     Username: ${document.getElementById('userName').value}
-//     Password: ${document.getElementById('password').value}
-//     Email: ${document.getElementById('email').value}
-//     Phone Number: ${document.getElementById('phoneNumber').value}
-//     City: ${document.getElementById('cityMenu').value}
-//    `;
-//     my_form.reset() //resets the form
-// }
+})
